78bc7566cbf2f6bde6ba777a5735da0594b3e55a
RalphR <ralphr@interworldhighway.com>
2021-12-16 11:24:19 -0500

    initial commit

---
+++ ContosaCrafts.WebSite/Controller/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using ContosaCrafts.WebSite.Models;
+using ContosaCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosaCrafts.WebSite.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productsService)
+        {
+            this.ProductService = productsService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string productId, [FromQuery] int Rating)
+        {
+            ProductService.AddRating(productId, Rating);
+            return Ok();
+        }
+    }
+}
